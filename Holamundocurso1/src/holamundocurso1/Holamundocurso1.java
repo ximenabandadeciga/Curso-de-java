c9c11fe9a3fb1e59a7d2517276eb61cc3933e537
Ximena Banda Déciga <74206546+ximenabandadeciga@users.noreply.github.com>
2021-02-18 13:53:58 -0600

    Add files via upload

---
+++ Holamundocurso1/src/holamundocurso1/Holamundocurso1.java
@@ -0,0 +1,22 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundocurso1;
+
+/**
+ *
+ * @author Ximena Banda Déciga
+ */
+public class Holamundocurso1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
