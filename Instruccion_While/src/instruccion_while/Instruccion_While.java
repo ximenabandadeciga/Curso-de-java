169d19f46fa4df27d0b4d7c9b13dbb39ada45ed1
Ximena Banda Déciga <74206546+ximenabandadeciga@users.noreply.github.com>
2021-02-18 16:31:39 -0600

    Add files via upload

---
+++ Instruccion_While/src/instruccion_while/Instruccion_While.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instruccion_while;
+
+/**
+ *
+ * @author Ximena Banda Deciga
+ */
+public class Instruccion_While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //PRIMER EJEMPLO:
+        
+        //boolean cond=false;
+        //int x=0;
+        
+        //while(cond==false)
+        //{
+        
+            // System.out.println("El valor de x es: "+x);
+            // x = x + 2;
+            //if(x>8)
+            //{
+            // cond = true;
+            //}
+       //}
+       
+       //SEGUNDO EJEMPLO:
+       
+       int x=0;
+       while(x<=15)
+       {
+           System.out.println("El valor de x es:"+x);
+           x = (x*2) +3;
+       }
+       
+        
+        
+        
+    }
+    
+}
