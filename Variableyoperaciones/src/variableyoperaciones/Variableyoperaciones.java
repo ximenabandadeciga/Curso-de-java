c2db55d34a54336a10fa28371ebb4932d802cd41
Ximena Banda Déciga <74206546+ximenabandadeciga@users.noreply.github.com>
2021-02-18 13:57:58 -0600

    Add files via upload

---
+++ Variableyoperaciones/src/variableyoperaciones/Variableyoperaciones.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableyoperaciones;
+
+/**
+ *
+ * @author Ximena Banda Déciga
+ */
+public class Variableyoperaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       short suma;
+       suma = 3+10;
+       int resta = 8 -17;
+       long residuo =9/2;
+       float mutiplicacion = 2*(15*(-2));
+       double division = 10/3.4;
+       
+       System.out.println("El resultado de la suma es: "+suma);
+       System.out.println("El resultado de la resta es: "+resta);
+       System.out.println("El resultado de la multiplicacion es: "+mutiplicacion);
+       System.out.println("El resultado de la division es: "+division);
+       System.out.println("El resultado de el residuo es: "+residuo);
+       
+       
+       
+       
+    }
+    
+}
