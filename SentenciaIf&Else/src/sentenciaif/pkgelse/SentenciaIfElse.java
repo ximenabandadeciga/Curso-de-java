04da3ce62ca076c2a53f88537fa647237650308b
Ximena Banda Déciga <74206546+ximenabandadeciga@users.noreply.github.com>
2021-02-18 16:05:48 -0600

    Add files via upload

---
+++ SentenciaIf&Else/src/sentenciaif/pkgelse/SentenciaIfElse.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sentenciaif.pkgelse;
+
+/**
+ *
+ * @author Ximena Banda Deciga
+ */
+
+//operadres logicos >, >=, <=, ==, ll, &&, ! 
+public class SentenciaIfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int var = 6;
+        int var2 = 10;
+        
+        if(var<=6)
+        {
+            System.out.println("La operacion es verdadera");
+            
+        }
+        else
+        {
+            System.out.println("La operacion es falsa");
+            
+        }
+    }
+    
+}
