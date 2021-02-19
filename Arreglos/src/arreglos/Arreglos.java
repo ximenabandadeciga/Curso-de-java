b638fee36b02e61309f0f53e57e4afb86bbaef5b
Ximena Banda Déciga <74206546+ximenabandadeciga@users.noreply.github.com>
2021-02-19 10:21:40 -0600

    Add files via upload

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Ximena Banda Deciga
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       int x[] = {10,5,7}; // new int [3];
+       
+       /*
+       x[0] = 10;
+       x[1] = 5;
+       x[2] = 7;
+       */
+       for(int i=0; i<x.length;i++)
+       {
+            System.out.println("Arreglo en indice:"+i+ "es igual a:"+x[i]);
+       }
+    }
+
+}
+    
+
